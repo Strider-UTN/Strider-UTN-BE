420a4fa6abecda7f450889249696770d1fbb729b
jmosmann <jmosmann@makingsense.com>
2025-07-28 23:36:02 -0300

    Implemented the AuthService and created an example service. Added configuration to AppSettings. Configured EF and Tokens in program.cs.

---
+++ StriderBE/StriderWebApi/Data/StriderDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace StriderWebApi.Data
+{
+    public class StriderDbContext : DbContext
+    {
+        #region Constructors
+        public StriderDbContext(DbContextOptions<StriderDbContext> options) : base(options) {}
+        #endregion
+
+        #region DbSets
+        #endregion
+    }
+}
---
+++ StriderBE/StriderWebApi/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using StriderWebApi.Data;
+using StriderWebApi.Services;
+using StriderWebApi.Services.Interfaces;
+using System.Text;
+
+namespace StriderWebApi.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtSettings = configuration.GetSection("Jwt");
+            var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+
+                    ValidIssuer = jwtSettings["Issuer"],
+                    ValidAudience = jwtSettings["Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                };
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddProjectServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // DbContext
+            services.AddDbContext<StriderDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("StriderConnectionString")));
+
+            // Services
+            services.AddScoped<IAuthService, AuthService>();
+
+            return services;
+        }
+    }
+}
